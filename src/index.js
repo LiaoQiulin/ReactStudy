6934baa839f08e56754d58b59880dec3b9131fcc
liao qiulin <1286936364@qq.com>
2022-03-12 15:59:26 +0800

    hello word

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// const name = 'Josh Perez';
+// const element = <h1>Hello, {name}</h1>;
+
+// ReactDOM.render(
+//     element,
+//     document.getElementById('root')
+// );
+
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+    firstName: 'Harper1',
+    lastName: 'Perez1'
+};
+
+const element = (
+    <h1>
+        Hello, {formatName(user)}!
+    </h1>
+);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    element,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
