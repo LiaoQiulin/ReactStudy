4b39c8eb6202d93a2d932c0de2655a06eebb53c4
liao qiulin <1286936364@qq.com>
2022-03-20 10:22:39 +0800

    Components and Props

---
+++ src/ComposingComponents.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+
+export default () => {
+
+    function Welcome(props) {
+        return <h1>Hello, {props.name}</h1>;
+    }
+
+    function App() {
+        return (
+            <div>
+                <Welcome name="Sara"/>
+                <Welcome name="Cahal"/>
+                <Welcome name="Edite"/>
+            </div>
+        );
+    }
+
+    ReactDOM.render(
+        <App/>,
+        document.getElementById('root')
+    );
+
+}
+
---
+++ src/WelcomeClass.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+export default ()=>{
+    class Welcome extends React.Component {
+        render() {
+            return <h1>Hello, {this.props.name}</h1>;
+        }
+    }
+    const element = <Welcome name="Sara" />;
+
+    ReactDOM.render(
+        element,
+        document.getElementById('root')
+    );
+}
+
