434ca4438ea81600a47463335851fa55b9da4073
liao qiulin <1286936364@qq.com>
2022-04-04 19:57:15 +0800

    组件和属性

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import reportWebVitals from './reportWebVitals';
-// import run from "./WelcomeFun";
+import run from "./WelcomeFun";
 // import run from "./WelcomeClass";
 // import run from './ComposingComponents';
+// import run from './ExtractingComponentsBefore';
 // import run from './ExtractingComponentsAfter';
-import run from './ExtractingComponentsAfter';
 
 run()
 
