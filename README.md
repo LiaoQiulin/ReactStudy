b86cee79180b740f00d1f338a023ce17d7fe0784
liao qiulin <1286936364@qq.com>
2022-04-11 22:52:00 +0800

    hooks规则

--- README.md
+++ README.md
@@ -1,44 +1,12 @@
-#  useEffect
-## 0. 效果钩子介绍
-> Effect Hook 允许您在函数组件中执行副作用
+# Hooks使用规则
 
->在 React 组件中获取数据、设置订阅和手动更改 DOM 都是副作用的示例。无论您是否习惯将这些操作称为“副作用”（或仅称为“效果”），您之前都可能在组件中执行过它们。
-## 1. 无需清理的效果 ``EffectsWithoutCleanup.js``
+## 1. 仅在顶层调用 Hooks
+>不要在循环、条件或嵌套函数中调用 Hook。
+>
+``So how does``
+>那么 React 是如何知道哪个 state 对应哪个 useState 调用的呢？答案是 React 依赖于 Hooks 的调用顺序。
+## 2. 仅从 React 函数调用 Hooks
 
-```jsx
-useEffect(() => {
-    console.log("组件初始化或更新")
-});
-```
-### 1.1 useEffect 有什么作用？
->通过使用这个 Hook，你告诉 React 你的组件需要在渲染之后做一些事情。
-### 1.2 为什么在组件内部调用 useEffect？
->将 useEffect 放在组件中可以让我们直接从效果中访问计数状态变量（或任何属性）。
-### 1.3 每次渲染后都会运行 useEffect 吗？
->是的！默认情况下，它会在第一次渲染后和每次更新后运行。
-
-## 2. 带清理的效果  ``EffectsWithCleanup.js``
-### 2.1 为什么我们从效果中返回一个函数？
-> 这是效果的可选清理机制。每个效果都可能返回一个在它之后进行清理的函数。
-### 2.2 React 究竟何时清理效果？
-> React 在组件卸载时执行清理。然而，正如我们之前所了解的，效果会为每次渲染运行，而不仅仅是一次。这就是为什么 React 还会在下次运行效果之前清理上一次渲染中的效果。
-## 3 回顾
-## 4 使用效果的技巧
-### 4.1 使用多个效果来区分关注点
-### 4.2 通过跳过效果优化性能
->将数组作为可选的第二个参数传递给 useEffect
-```jsx
-        useEffect(() => {
-            console.log("组件初始化或更新: 空数组")
-            return function (){
-                console.log("组件销毁: 空数组")
-            }
-        },[]);
-       useEffect(() => {
-            console.log("组件初始化或更新: count")
-            return function (){
-                console.log("组件销毁: count")
-            }
-        },[count]);
-```
-**数组中的值没有变动, 重新渲染时,会跳过此效果.**
+>不要从常规 JavaScript 函数中调用 Hooks。相反，您可以：
+1. 从 React 函数组件调用 Hooks。
+2. 从自定义 Hooks 调用 Hooks
--- src/index.js
+++ src/index.js
@@ -1,11 +1,46 @@
 import reportWebVitals from './reportWebVitals';
 
-// import run from './EffectsWithoutCleanup'
-// import run from './EffectsWithoutCleanupClass'
-// import run from './EffectsWithCleanup'
-// import run from './EffectsWithCleanupClass'
-import run from './SkippingEffects'
+import ReactDOM from "react-dom";
+import React, {useEffect, useState} from "react";
 
-run();
+function Form() {
+    // 1. Use the name state variable
+    const [name, setName] = useState('Mary');
+
+    // 2. Use an effect for persisting the form
+    useEffect(function persistForm() {
+        localStorage.setItem('formData', name);
+    });
+    // if (name !== '') {
+    //     useEffect(function persistForm() {
+    //         localStorage.setItem('formData', name);
+    //     });
+    // }
+    useEffect(function persistForm() {
+        // 👍 We're not breaking the first rule anymore
+        if (name !== '') {
+            localStorage.setItem('formData', name);
+        }
+    });
+
+
+    // 3. Use the surname state variable
+    const [surname, setSurname] = useState('Poppins');
+
+    // 4. Use an effect for updating the title
+    useEffect(function updateTitle() {
+        document.title = name + ' ' + surname;
+    });
+    return (<>
+        <h4>{name}</h4>
+        <button onClick={() => setName("")}>a=""</button>
+    </>)
+    // ...
+}
+
+ReactDOM.render(
+    <Form/>,
+    document.getElementById('root')
+);
 
 reportWebVitals();
