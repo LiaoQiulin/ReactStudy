437a056a6c2ce30ca98ffba1aaa13670647cf1a7
liao qiulin <1286936364@qq.com>
2022-04-20 22:26:38 +0800

    Add Some Routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import reportWebVitals from './reportWebVitals';
 // import run from './01Installation'
 // import run from './02ConnectTheURL'
-import run from './03AddSomeLinks'
+// import run from './03AddSomeLinks'
+import run from './04AddSomeRoutes'
 
 run()
 
