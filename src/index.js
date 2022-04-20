b9e7a55ecdf76ac71d9c530655399192ea5a2691
liao qiulin <1286936364@qq.com>
2022-04-20 23:04:05 +0800

    Revert "Listing the Invoices"
    This reverts commit f38fd71f13679f3750826d291ee60635dc2c00ce.

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import reportWebVitals from './reportWebVitals';
 // import run from './02ConnectTheURL'
 // import run from './03AddSomeLinks'
 // import run from './04AddSomeRoutes'
-// import run from './05NestedRoutes'
-import run from './06ListingTheInvoices'
+import run from './05NestedRoutes'
 
 run()
 
