8cbf25e41b23b6263be09b1868e6c0cf5269a2af
liao qiulin <1286936364@qq.com>
2022-06-05 01:18:07 +0800

    代码分割

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
-import React, { useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { Suspense, useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import RequireAuth, { AuthProvider } from './component/Auth'
-import ProtectedPage from './page/ProtectedPage'
-import PublicPage from './page/PublicPage'
-import LoginPage from './page/LoginPage'
-import NoMatchPage from './page/NoMatchPage'
 
 function RedirectHome() {
     let navigate = useNavigate();
-
     useEffect(() => {
         navigate("/public", { replace: true })
     }, [])
     return <></>
+}
+const PublicPage = React.lazy(() => import('./page/PublicPage'));
+const ProtectedPage = React.lazy(() => import('./page/ProtectedPage'));
+const LoginPage = React.lazy(() => import('./page/LoginPage'));
+const NoMatchPage = React.lazy(() => import('./page/NoMatchPage'));
+
+function Lazy(props) {
+    return <Suspense fallback={<>Loing</>}>{props.children}</Suspense>
 }
 
 export default function App() {
     return (<AuthProvider>
         <Routes>
             <Route path="/" element={<RedirectHome />} />
-            <Route path="/public" element={<PublicPage />} />
-            <Route path="/login" element={<LoginPage />} />
+            <Route path="/public" element={<Lazy><PublicPage /></Lazy>} />
+            <Route path="/login" element={<Lazy><LoginPage /></Lazy>} />
             <Route
                 path="/protected"
                 element={
                     <RequireAuth>
-                        <ProtectedPage />
+                        <Lazy><ProtectedPage /></Lazy>
                     </RequireAuth>
                 }
             />
-            <Route path="*" element={<NoMatchPage />} />
+            <Route path="*" element={<Lazy><NoMatchPage /></Lazy>} />
         </Routes>
 
     </AuthProvider>)
