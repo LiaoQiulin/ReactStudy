d582fc858fa95a1ee5bd91f0dc85ec6d16df8764
liao qiulin <1286936364@qq.com>
2022-06-05 00:31:07 +0800

    1 多环境配置 2 nginx子路径部署

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import React from 'react';
 import App from "./App";
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App/>
+    <BrowserRouter basename='/app'>
+        <App />
     </BrowserRouter>,
     document.getElementById('root')
 );
+
+//     location = /app {
+//     root /var/www/myapp/build;
+//     try_files /index.html =404;
+//     }
+//     location ~ ^/app(.*) {
+//     root /var/www/myapp/build;
+//     try_files $1 $1/ /index.html =404;
+//     }
 
 reportWebVitals();
